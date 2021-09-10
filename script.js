ddd5b0b9053c7ab50af14c7418640913c5661059
nabeel muhammed <chirakkalnabeel@gmail.com>
2021-09-10 10:34:11 +0530

    msg

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("hello world");
+
+console.log("");
+var company=("nabeel muhammed");
+var loc_name=("kaakanad");
+console.log("age : 21");
+console.log("Nabeel muhammed");
+console.log()
+var location_name="kakkanad",company_name="luminar technolab";
+
+console.log('')
